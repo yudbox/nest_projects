0e320e0a3dab4761df2aa40911346a4306504ef0
Aleksandr <a.yudaiev@ossystem.ua>
2021-11-05 18:04:05 +0200

    #first-api auth module passport and JWT strategy

--- nest-first-api/src/app.module.ts
+++ nest-first-api/src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-const environment = process.env.NODE_ENV || 'development';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+import { configModule } from './configure.root';
+import { TokenModule } from './token/token.module';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
-    ConfigModule.forRoot({
-      envFilePath: `.env.${environment}`,
-      isGlobal: true,
-    }),
+    configModule,
     MongooseModule.forRoot(process.env.MONGODB_WRITE_CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
+    TokenModule,
   ],
 })
 export class AppModule {}
--- nest-first-api/src/auth/auth.service.ts
+++ nest-first-api/src/auth/auth.service.ts
@@ -1,13 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { TokenService } from 'src/token/token.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { SignOptions } from 'jsonwebtoken';
+import { CreateUserTokenDto } from '../token/dto/create-user-token.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+    private readonly tokenService: TokenService,
+  ) {
     const mongoConnectionStr = this.configService.get<string>(
       'MONGODB_WRITE_CONNECTION_STRING',
     );
 
     console.log('111 mongoConnectionStr', mongoConnectionStr);
+  }
+
+  signUp(createUserDto: CreateUserDto) {}
+
+  signIn(email, password) {}
+
+  private async generateToken(data, options?: SignOptions): Promise<string> {
+    return this.jwtService.sign(data, options);
+  }
+
+  private async verifyToken(token): Promise<any> {
+    try {
+      const data = this.jwtService.verify(token);
+      const tokenExists = await this.tokenService.exists(data._id, token);
+
+      if (tokenExists) {
+        return data;
+      }
+      throw new UnauthorizedException();
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private async saveToken(createUserTokenDto: CreateUserTokenDto) {
+    const userToken = await this.tokenService.create(createUserTokenDto);
+
+    return userToken;
   }
 }
