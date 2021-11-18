8e17be73ad3b3688c41697ee9a829d29717187bd
Aleksandr <a.yudaiev@ossystem.ua>
2021-11-18 17:14:01 +0200

    #product-api auth changes

--- nest-product-card-api/src/auth/auth.module.ts
+++ nest-product-card-api/src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
+import { AuthSchema } from './schemas/auth.schema';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [MongooseModule.forFeature([{name: 'Auth', schema: AuthSchema}])],
 })
 export class AuthModule {}
---
+++ nest-product-card-api/src/auth/schemas/auth.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema()
+export class Auth {
+  @Prop()
+  _id: string;
+
+  @Prop({unique: true})
+  email: string;
+
+  @Prop()
+  passwordHash: number;
+
+  @Prop({type: Date, required: true})
+  createdAt: Date;
+
+
+}
+
+export const AuthSchema = SchemaFactory.createForClass(Auth);
